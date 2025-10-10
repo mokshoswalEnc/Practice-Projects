b2035c631736fda7058148a8d0c78b26e473578a
Moksh Oswal <moksh.oswal@encora.com>
2025-10-10 12:19:46 +0530

    kafka projects

---
+++ RetailProject/OrderService33/src/main/java/com/order/OrderService33Application.java
@@ -0,0 +1,13 @@
+package com.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderService33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderService33Application.class, args);
+	}
+
+}

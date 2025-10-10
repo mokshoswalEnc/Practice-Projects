e21b657b490d64104bb531ff166dd774034e30ec
Moksh Oswal <moksh.oswal@encora.com>
2025-10-10 12:10:21 +0530

    Synchronous Communication

---
+++ RestTemplateCommunicationProject/Emp-Employee-service/src/test/java/com/EmpTest/EmpEmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmpTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpEmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ RestTemplateCommunicationProject/Emp-order-service/src/test/java/com/orderTest/EmpOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

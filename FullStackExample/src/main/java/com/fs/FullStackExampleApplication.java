2f0de3ba998537fa3832d30c9205cf4946433204
Moksh Oswal <moksh.oswal@encora.com>
2025-10-27 12:37:45 +0530

    Full stack basic project

---
+++ FullStackExample/src/main/java/com/fs/FullStackExampleApplication.java
@@ -0,0 +1,13 @@
+package com.fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackExampleApplication.class, args);
+	}
+
+}
